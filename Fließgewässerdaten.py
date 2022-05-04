9ada63a51d6928307dfd8cec6f4fa06daa77c7ee
lmashni <laithmashni@gmail.com>
2022-05-04 13:19:17 +0200

    argparse

--- Fließgewässerdaten.py
+++ Fließgewässerdaten.py
@@ -12,35 +12,43 @@ from matplotlib import pyplot as plt
 import datetime 
 import time
 import matplotlib.dates as md 
+import argparse
 
+def arguments_parser():
+	parser = argparse.ArgumentParser(description="This graphs data about water quality in wendlingen")
+	parser.add_argument('-d','--days',default = 365*3, type=int,help="number of days to fetch data from ending with latest data point. default is set to three years. minimum is one day")
+	
+	return parser.parse_args()
+		
+args = arguments_parser()
+days = args.days
+
+dt = 3600000*24*days
 t0 = int(time.time()*1000) # current time
-t1 = t0 -3600000*24*30*12*5 # how far back you want to go
+t1 = t0 -dt # how far back you want to go
 
-
-leb = ['temp','o2','ph','lf','tr']#,'temp']
-#url = 'https://lupo-cloud.de/water/metric/de.lubw.gewaesser.temp?labels=station:2875,type:measurement&from=1644307200000&to=16449120000001'
+leb = ['temp','o2','ph','lf','tr']
+url = 'https://lupo-cloud.de/water/metric/de.lubw.gewaesser.'
 data=[]
-C = ['r','b','g','y','k']
-E = ['°c','[mg/l]','pH',r'$\mu$ S/cm','[FNU]']
-L = ['Temp.','o2','pH','Leitf.','Trüb.' ]
 
 for i in leb:
-    res = requests.get('https://lupo-cloud.de/water/metric/de.lubw.gewaesser.'+i+'?labels=station:2875&from='+str(t1)+'&to='+str(t0))
+    res = requests.get(url+i+'?labels=station:2875&from='+str(t1)+'&to='+str(t0))
     data.append(json.loads(res.text))
 
 fig,ax =plt.subplots(len(leb),sharex=True)
+fig.suptitle('Fließgewässerdaten Wendlingen \n letzte Datenpunkt  '+str(datetime.datetime.fromtimestamp(data[0][-1]['times'][-1]/1000)))    
+xfmt = md.DateFormatter('%m-%d')#'%H:%M')
+ax[-1].xaxis.set_major_formatter(xfmt)
 
-
+C = ['r','b','g','y','k']
+E = ['°c','[mg/l]','pH',r'$\mu$ S/cm','[FNU]']
+L = ['Temp.','o2','pH','Leitf.','Trüb.' ]
 
 for i,j,c,e in zip(range(len(leb)),L,C,E):
     ax[i].plot([datetime.datetime.fromtimestamp(k/1000) for k in data[i][-1]['times']],data[i][-1]['values'],c,label=j)
     ax[i].legend(loc=2)
     ax[i].set_ylabel(e)
-fig.suptitle('Fließgewässerdaten Wendlingen \n letzte Datenpunkt  '+str(datetime.datetime.fromtimestamp(data[i][-1]['times'][-1]/1000)))    
-
-xfmt = md.DateFormatter('%m-%d')#'%H:%M')
-
-ax[-1].xaxis.set_major_formatter(xfmt)
-print(datetime.datetime.fromtimestamp(data[3][-1]['times'][-1]/1000))
+    
+print('last data point', datetime.datetime.fromtimestamp(data[3][-1]['times'][-1]/1000))
 
 plt.show()
